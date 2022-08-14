a689fce6d080db9adcba87d52a80df45c43bf75c
Oli Dacombe <odacombe@blockchain.com>
2022-08-14 16:22:37 +0100

    swtpm as container

--- Makefile
+++ Makefile
@@ -1,7 +1,15 @@
-.PHONY: init readme
+.PHONY: init readme swtpm swtpm-image
 
 init:
 	cargo install cargo-readme
 
 readme:
 	cargo readme > README.md
+
+swtpm-image:
+	cd tests \
+	&& docker build -t swtpm -f Dockerfile-swtpm . \
+
+swtpm: swtpm-image
+	-docker kill swtpm
+	docker run -d --rm --name swtpm -p 2321:2321 -p 2322:2322 swtpm
---
+++ tests/swtpm-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+############################
+# Run the TPM SWTPM server #
+############################
+mkdir /tmp/tpmdir
+swtpm_setup --tpm2 \
+    --tpmstate /tmp/tpmdir \
+    --createek --decryption --create-ek-cert \
+    --create-platform-cert \
+    --pcr-banks sha1,sha256 \
+    --display
+swtpm socket --tpm2 \
+    --tpmstate dir=/tmp/tpmdir \
+    --flags startup-clear \
+    --ctrl type=tcp,port=2322,bindaddr=0.0.0.0 \
+    --server type=tcp,port=2321,bindaddr=0.0.0.0 \
+    #--daemon
+#tpm2-abrmd \
+    #--logger=stdout \
+    #--tcti=swtpm: \
+    #--allow-root \
+    #--session \
+    #--flush-all
