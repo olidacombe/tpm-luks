a7a9d9f575d2107d723e1f0be55e60c087515eca
Oli Dacombe <odacombe@blockchain.com>
2022-08-15 20:20:02 +0100

    start with some cleanup

--- README.md
+++ README.md
@@ -2,18 +2,14 @@
 
 ## Get Started
 
+### On Linux with hardware TPM:
 ```bash
-# at basecamp
-fswatch -o . | xargs -n1 -I{} ./sync.sh {machine:dir}
-# on remote (e.g. Metal) machine
-# TCTI=device:/dev/tpm0 cargo watch -x "test -- --nocapture"
-mkdir /tmp/mytpm
-chown tss:root /tmp/mytpm
-swtpm_setup --overwrite --tpmstate /tmp/mytpm/ --create-ek-cert --create-platform-cert --tpm2 \
---lock-nvram
-swtpm socket --tpmstate dir=/tmp/mytpm --tpm2 --ctrl type=tcp,port=2322 --log level=20 --server type=tcp,port=2321
-tpm2_startup --tcti swtpm -c
-TCTI=swtpm:port=2321,host=127.0.0.1  cargo watch -x "test -- --nocapture"
+make dev-local
+```
+
+### On something else, e.g. Mac:
+```bash
+make dev
 ```
 
 License: MIT OR Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,18 @@
 //! # Get Started
 //!
+//! ## On Linux with hardware TPM:
 //! ```bash
-//! # at basecamp
-//! fswatch -o . | xargs -n1 -I{} ./sync.sh {machine:dir}
-//! # on remote (e.g. Metal) machine
-//! # TCTI=device:/dev/tpm0 cargo watch -x "test -- --nocapture"
-//! mkdir /tmp/mytpm
-//! chown tss:root /tmp/mytpm
-//! swtpm_setup --overwrite --tpmstate /tmp/mytpm/ --create-ek-cert --create-platform-cert --tpm2 \
-//! --lock-nvram
-//! swtpm socket --tpmstate dir=/tmp/mytpm --tpm2 --ctrl type=tcp,port=2322 --log level=20 --server type=tcp,port=2321
-//! tpm2_startup --tcti swtpm -c
-//! TCTI=swtpm:port=2321,host=127.0.0.1  cargo watch -x "test -- --nocapture"
+//! make dev-local
+//! ```
+//!
+//! ## On something else, e.g. Mac:
+//! ```bash
+//! make dev
 //! ```
 
 use once_cell::sync::Lazy;
-use std::sync::{Arc, Mutex};
-use tss_esapi::{tcti_ldr::TctiNameConf, Context, Result as TSSResult};
+use std::sync::Mutex;
+use tss_esapi::{tcti_ldr::TctiNameConf, Context};
 
 static CONTEXT: Lazy<Mutex<Context>> = Lazy::new(|| {
     Mutex::new(Context::new(TctiNameConf::from_environment_variable().unwrap()).unwrap())
@@ -26,12 +22,9 @@ static CONTEXT: Lazy<Mutex<Context>> = Lazy::new(|| {
 mod tests {
     use super::*;
     use eyre::Result;
-    use tss_esapi::{
-        interface_types::algorithm::HashingAlgorithm, structures::SymmetricDefinition,
-    };
 
     #[test]
-    fn yada() -> Result<()> {
+    fn list_nvs() -> Result<()> {
         use tss_esapi::abstraction::nv::list;
         let mut context = CONTEXT.lock().unwrap();
         let nvs = list(&mut context)?;
@@ -43,44 +36,30 @@ mod tests {
         Ok(())
     }
 
-    //https://tpm2-software.github.io/2020/04/13/Disk-Encryption.html#pcr-policy-authentication---access-control-of-sealed-pass-phrase-on-tpm2-with-pcr-sealing
     #[test]
-    fn seal_101() -> Result<()> {
-        use tss_esapi::attributes::object::ObjectAttributesBuilder;
+    fn get_revision() -> Result<()> {
         use tss_esapi::constants::property_tag::PropertyTag;
-        use tss_esapi::constants::session_type::SessionType;
-        use tss_esapi::interface_types::algorithm::PublicAlgorithm;
-        use tss_esapi::interface_types::resource_handles::Hierarchy;
-        use tss_esapi::interface_types::session_handles::PolicySession;
-        use tss_esapi::structures::{
-            Digest, KeyedHashScheme, MaxBuffer, PublicBuilder, PublicKeyedHashParameters,
-        };
-        // Create a key
-        let object_attributes = ObjectAttributesBuilder::new()
-            .with_sign_encrypt(true)
-            .with_sensitive_data_origin(true)
-            .with_user_with_auth(true)
-            .build()
-            .expect("Failed to build object attributes");
-        let key_pub = PublicBuilder::new()
-            .with_public_algorithm(PublicAlgorithm::KeyedHash)
-            .with_name_hashing_algorithm(HashingAlgorithm::Sha256)
-            .with_object_attributes(object_attributes)
-            .with_keyed_hash_parameters(PublicKeyedHashParameters::new(
-                KeyedHashScheme::HMAC_SHA_256,
-            ))
-            .with_keyed_hash_unique_identifier(Digest::default())
-            .build()
-            .unwrap();
         let mut context = CONTEXT.lock().unwrap();
 
-        let rev = context
+        context
             .get_tpm_property(PropertyTag::Revision)
             .expect("Wrong value from TPM")
             .expect("Value is not supported");
-        assert_eq!(rev, 164);
 
-        let session: PolicySession = context
+        Ok(())
+    }
+
+    //https://tpm2-software.github.io/2020/04/13/Disk-Encryption.html#pcr-policy-authentication---access-control-of-sealed-pass-phrase-on-tpm2-with-pcr-sealing
+    #[test]
+    fn seal() -> Result<()> {
+        use tss_esapi::constants::SessionType;
+        use tss_esapi::interface_types::{
+            algorithm::HashingAlgorithm, session_handles::PolicySession,
+        };
+        use tss_esapi::structures::SymmetricDefinition;
+        let mut context = CONTEXT.lock().unwrap();
+
+        let _session: PolicySession = context
             .start_auth_session(
                 None,
                 None,
@@ -92,53 +71,7 @@ mod tests {
             .expect("Failed to create session")
             .expect("Received invalid handle")
             .try_into()?;
+
         Ok(())
     }
-
-    //#[test]
-    //fn bla() {
-    //use tss_esapi::{
-    //interface_types::algorithm::HashingAlgorithm,
-    //structures::{PcrSelectionListBuilder, PcrSlot},
-    //tcti_ldr::TctiNameConf,
-    //Context,
-    //};
-    //let mut context =
-    //Context::new(TctiNameConf::from_environment_variable().expect("Failed to get TCTI"))
-    //.expect("Failed to create Context");
-    //// Create PCR selection list with slots in a bank
-    //// that is going to be read.
-    //let pcr_selection_list = PcrSelectionListBuilder::new()
-    //.with_selection(
-    //HashingAlgorithm::Sha256,
-    //&[
-    //PcrSlot::Slot0,
-    //PcrSlot::Slot1,
-    //PcrSlot::Slot2,
-    //PcrSlot::Slot3,
-    //PcrSlot::Slot4,
-    //PcrSlot::Slot5,
-    //PcrSlot::Slot6,
-    //PcrSlot::Slot7,
-    //PcrSlot::Slot8,
-    //PcrSlot::Slot9,
-    //PcrSlot::Slot10,
-    //PcrSlot::Slot11,
-    //PcrSlot::Slot12,
-    //PcrSlot::Slot13,
-    //PcrSlot::Slot14,
-    //PcrSlot::Slot15,
-    //PcrSlot::Slot16,
-    //PcrSlot::Slot17,
-    //PcrSlot::Slot18,
-    //PcrSlot::Slot19,
-    //PcrSlot::Slot20,
-    //PcrSlot::Slot21,
-    //],
-    //)
-    //.build()
-    //.expect("Failed to build PcrSelectionList");
-    ////let _pcr_data = tss_esapi::abstraction::pcr::read_all(&mut context, pcr_selection_list)
-    ////.expect("pcr::read_all failed");
-    //}
 }
