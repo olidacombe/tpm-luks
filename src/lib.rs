e6e4d90581cf89c263e3eab5ba5bf712420d2129
Oli Dacombe <odacombe@blockchain.com>
2022-09-23 23:45:04 +0100

    fail on the macro for pcr_slot_to_handle

--- src/lib.rs
+++ src/lib.rs
@@ -101,7 +101,40 @@ static CONTEXT: Lazy<Mutex<tss_esapi::Context>> = Lazy::new(|| {
 });
 
 fn pcr_slot_to_handle(slot: &PcrSlot) -> PcrHandle {
-    PcrHandle::Pcr0
+    match slot {
+        PcrSlot::Slot0 => PcrHandle::Pcr0,
+        PcrSlot::Slot1 => PcrHandle::Pcr1,
+        PcrSlot::Slot2 => PcrHandle::Pcr2,
+        PcrSlot::Slot3 => PcrHandle::Pcr3,
+        PcrSlot::Slot4 => PcrHandle::Pcr4,
+        PcrSlot::Slot5 => PcrHandle::Pcr5,
+        PcrSlot::Slot6 => PcrHandle::Pcr6,
+        PcrSlot::Slot7 => PcrHandle::Pcr7,
+        PcrSlot::Slot8 => PcrHandle::Pcr8,
+        PcrSlot::Slot9 => PcrHandle::Pcr9,
+        PcrSlot::Slot10 => PcrHandle::Pcr10,
+        PcrSlot::Slot11 => PcrHandle::Pcr11,
+        PcrSlot::Slot12 => PcrHandle::Pcr12,
+        PcrSlot::Slot13 => PcrHandle::Pcr13,
+        PcrSlot::Slot14 => PcrHandle::Pcr14,
+        PcrSlot::Slot15 => PcrHandle::Pcr15,
+        PcrSlot::Slot16 => PcrHandle::Pcr16,
+        PcrSlot::Slot17 => PcrHandle::Pcr17,
+        PcrSlot::Slot18 => PcrHandle::Pcr18,
+        PcrSlot::Slot19 => PcrHandle::Pcr19,
+        PcrSlot::Slot20 => PcrHandle::Pcr20,
+        PcrSlot::Slot21 => PcrHandle::Pcr21,
+        PcrSlot::Slot22 => PcrHandle::Pcr22,
+        PcrSlot::Slot23 => PcrHandle::Pcr23,
+        PcrSlot::Slot24 => PcrHandle::Pcr24,
+        PcrSlot::Slot25 => PcrHandle::Pcr25,
+        PcrSlot::Slot26 => PcrHandle::Pcr26,
+        PcrSlot::Slot27 => PcrHandle::Pcr27,
+        PcrSlot::Slot28 => PcrHandle::Pcr28,
+        PcrSlot::Slot29 => PcrHandle::Pcr29,
+        PcrSlot::Slot30 => PcrHandle::Pcr30,
+        PcrSlot::Slot31 => PcrHandle::Pcr31,
+    }
 }
 
 impl Context {
