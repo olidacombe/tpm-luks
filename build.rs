4d89b4a5f4142ba3136804e0d844ab5690ff5b66
Oli Dacombe <odacombe@blockchain.com>
2022-10-08 13:46:01 +0100

    dependency pain

--- build.rs
+++ build.rs
@@ -1,6 +1,14 @@
 fn main() {
     // TODO wrap in env var condition
+    println!("cargo:rustc-link-search=/usr/lib");
     println!("cargo:rustc-link-search=/usr/local/ssl/lib");
     println!("cargo:rustc-link-lib=static=crypto");
+    println!("cargo:rustc-link-lib=static=cryptsetup");
+    //println!("cargo:rustc-link-lib=static=json-c");
     println!("cargo:rustc-link-lib=static=ssl");
+    //println!("cargo:rustc-link-lib=static=tss2-esys");
+    //println!("cargo:rustc-link-lib=static=tss2-mu");
+    //println!("cargo:rustc-link-lib=static=tss2-rc");
+    //println!("cargo:rustc-link-lib=static=tss2-sys");
+    //println!("cargo:rustc-link-lib=static=tss2-tctildr");
 }
