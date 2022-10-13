158f094f87ffae60cd3e2a66e95e999c5d9f6cd6
Oli Dacombe <odacombe@blockchain.com>
2022-10-13 22:35:38 +0100

    .

--- build.rs
+++ build.rs
@@ -1,16 +1,16 @@
 fn main() {
     // TODO wrap in env var condition
     println!("cargo:rustc-link-arg=-l:libc.a");
-    println!("cargo:rustc-link-search=/usr/lib");
-    println!("cargo:rustc-link-search=/usr/local/ssl/lib");
     println!("cargo:rustc-link-lib=static=crypto");
     println!("cargo:rustc-link-lib=static=cryptsetup");
     println!("cargo:rustc-link-lib=static=devmapper");
     println!("cargo:rustc-link-lib=static=json-c");
     println!("cargo:rustc-link-lib=static=ssl");
-    println!("cargo:rustc-link-lib=static=uuid");
     println!("cargo:rustc-link-lib=static=tss2-esys");
     println!("cargo:rustc-link-lib=static=tss2-mu");
     println!("cargo:rustc-link-lib=static=tss2-sys");
     println!("cargo:rustc-link-lib=static=tss2-tctildr");
+    println!("cargo:rustc-link-lib=static=uuid");
+    println!("cargo:rustc-link-search=/usr/lib");
+    println!("cargo:rustc-link-search=/usr/local/ssl/lib");
 }
