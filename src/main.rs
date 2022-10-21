ba462e35ea60ce472c9d53c44cefbcef40113fd2
Oli Dacombe <odacombe@blockchain.com>
2022-10-21 23:13:21 +0100

    gate tss2 log suppression with env var

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ use eyre::Result;
 use std::env;
 use tpm_luks::cli::Cli;
 
+const TSS2_LOG: &str = "TSS2_LOG";
+
 fn main() -> Result<()> {
     let cli = Cli::new();
     env_logger::init_from_env(
         env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
     );
-    env::set_var("TSS2_LOG", "all+debug");
+    if env::var(TSS2_LOG).is_err() {
+        env::set_var(TSS2_LOG, "all+NONE");
+    }
     cli.run()?;
     Ok(())
 }
