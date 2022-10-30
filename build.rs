3794d5f5555a7ea609497ff2429ccf0191983aaf
Oli Dacombe <odacombe@blockchain.com>
2022-10-30 17:03:43 +0000

    parametrize static TCTI

--- build.rs
+++ build.rs
@@ -1,13 +1,12 @@
 fn main() {
+    let static_tcti = option_env!("TPM_LUKS_BUILD_STATIC_TCTI").map_or("device");
     if let Some(build_static) = option_env!("TPM_LUKS_BUILD_STATIC") {
         if matches!(
             build_static.to_lowercase().as_ref(),
             "1" | "y" | "yes" | "true"
         ) {
             println!("cargo:rustc-link-arg=-l:libc.a");
-            println!("cargo:rustc-link-arg=-l:libtss2-tcti-device.a");
-            //println!("cargo:rustc-link-arg=-l:libtss2-tcti-mssim.a");
-            //println!("cargo:rustc-link-arg=-l:libtss2-tcti-swtpm.a");
+            println!("cargo:rustc-link-arg=-l:libtss2-tcti-{}.a", static_tcti);
             println!("cargo:rustc-link-lib=static=crypto");
             println!("cargo:rustc-link-lib=static=cryptsetup");
             println!("cargo:rustc-link-lib=static=devmapper");
@@ -16,10 +15,6 @@ fn main() {
             println!("cargo:rustc-link-lib=static=tss2-esys");
             println!("cargo:rustc-link-lib=static=tss2-mu");
             println!("cargo:rustc-link-lib=static=tss2-sys");
-            //println!("cargo:rustc-link-lib=static=tss2-tcti-device");
-            //println!("cargo:rustc-link-lib=static=tss2-tcti-mssim");
-            //println!("cargo:rustc-link-lib=static=tss2-tcti-swtpm");
-            //println!("cargo:rustc-link-lib=static=tss2-tctildr");
             println!("cargo:rustc-link-lib=static=uuid");
             println!("cargo:rustc-link-search=/usr/lib");
             println!("cargo:rustc-link-search=/usr/local/ssl/lib");
